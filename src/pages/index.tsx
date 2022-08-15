c867b3606a04df3bafabe282a4d6d763a89b4a41
Julien Cros <juliencros@juliens-macbook-air.home>
2022-08-15 22:23:46 +0200

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ const Home: NextPage = () => {
       <div className="px-4 py-4"><Aizzord width={100} /></div>
       <div className='flex flex-col items-center'>
         <h1 className="font-black py-28 text-7xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Hey, I&apos;m Julien.</h1>
-        <p className="font-black py-4 text-5xl">I’m a French student,  web designer.</p>
-        <p className="font-black py-4 text-5xl">I’ll pass the test to start my studies at the school 42 in Paris.</p>
+        <p className="font-black py-4 text-5xl">I&apos;m a French student,  web designer.</p>
+        <p className="font-black py-4 text-5xl">I&apos;ll pass the test to start my studies at the school 42 in Paris.</p>
         <p className="font-black py-4 pb-32 text-5xl">hehe.</p>
       </div>
       {/* <div className=" border-solid rounded-xl mx-96 py-4 px-8 border-4 border-white "> */}
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
       {/* </div> */}
         <p className="font-black flex justify-center text-5xl pt-96 pb-10">My career</p>
         <div className="font-black flex flex-col pl-5 text-4xl">
-          <p className="border-b-4 border-t-4 border-white py-4 ml-5 mr-96"> 2018 - informatical class in Isaac de l'etoile.</p>
+          <p className="border-b-4 border-t-4 border-white py-4 ml-5 mr-96"> 2018 - informatical class in Isaac de l&apos;etoile.</p>
           <p className="border-b-4 border-white py-4 ml-5 mr-96"> 2021 - work for my own.</p>
           <p className="border-b-4 border-white py-4 ml-5 mr-96"> 2022 - pool in the school 42.</p>
        
